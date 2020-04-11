ec7366a0a2f240be13865e18ade0fa1b0c1b098f
Gina Vasiloff <Gina@PS-2388-US.local>
2020-04-11 10:27:14 -0600

    initial commit

---
+++ CommitSize/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Diagnostics;
+
+namespace CommitSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo()
+                {
+                    WorkingDirectory = "/Users/Gina/Workspace/AuditTrails",
+                    FileName = "git",
+                    Arguments = "log",
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
+                    UseShellExecute = false
+                };
+                Process proc = Process.Start(startInfo);
+                while(!proc.StandardOutput.EndOfStream)
+                {
+                    var line = proc.StandardOutput.ReadLine();
+                    Console.WriteLine(line);
+                }
+                proc.Close();              
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
